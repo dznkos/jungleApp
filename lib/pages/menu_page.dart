95bf318d82711366ddbdfea88a1c0f637b5d3e42
Luis Vice <khanccer@gmail.com>
2020-10-28 23:06:51 -0500

    version 1.0

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -4,7 +4,6 @@ import 'package:jungle_app/widgets/list_pizza.dart';
 class MenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     final size = MediaQuery.of(context).size;
 
     return Scaffold(
@@ -15,21 +14,39 @@ class MenuPage extends StatelessWidget {
       body: Container(
           child: ListView(
         children: [
-          ListPizza('Cheese Pizza'   ,'Descripcion pizza, Descripcion pizza, Descripcion pizza', 25.99, ''),
-          ListPizza('American Pizza' , 'Descripcion pizza, Descripcion pizza', 32.25, ''),
-          ListPizza('Italian Pizza'  , 'Descripcion pizza, Descripcion pizza', 19.00, ''),
-          ListPizza('Hawaiian Pizza' , 'Descripcion pizza, Descripcion pizza', 22.99, ''),
-          ListPizza('Romani Pizza'   , 'Descripcion pizza, Descripcion pizza', 25.99, ''),
-          ListPizza('France Pizza'   , 'Descripcion pizza, Descripcion pizza', 32.25, ''),
-          ListPizza('Irak Pizza'     , 'Descripcion pizza, Descripcion pizza', 19.00, ''),
-          ListPizza('China Pizza'    , 'Descripcion pizza, Descripcion pizza', 22.99, ''),
-          ListPizza('Hungria Pizza'  , 'Descripcion pizza, Descripcion pizza', 32.25, ''),
-          ListPizza('Hawaiian Pizza' , 'Descripcion pizza, Descripcion pizza', 19.00, ''),
-          ListPizza('Japan Pizza'    , 'Descripcion pizza, Descripcion pizza', 22.99, ''),
+          ListPizza(
+              'Cheese Pizza',
+              'Descripcion pizza, Descripcion pizza, Descripcion pizza',
+              25.99,
+              ''),
+          ListPizza('American Pizza', 'Descripcion pizza, Descripcion pizza',
+              32.25, ''),
+          ListPizza('Italian Pizza', 'Descripcion pizza, Descripcion pizza',
+              19.00, ''),
+          ListPizza('Hawaiian Pizza', 'Descripcion pizza, Descripcion pizza',
+              22.99, ''),
+          ListPizza('Romani Pizza', 'Descripcion pizza, Descripcion pizza',
+              25.99, ''),
+          ListPizza('France Pizza', 'Descripcion pizza, Descripcion pizza',
+              32.25, ''),
+          ListPizza(
+              'Irak Pizza', 'Descripcion pizza, Descripcion pizza', 19.00, ''),
+          ListPizza(
+              'China Pizza', 'Descripcion pizza, Descripcion pizza', 22.99, ''),
+          ListPizza('Hungria Pizza', 'Descripcion pizza, Descripcion pizza',
+              32.25, ''),
+          ListPizza('Hawaiian Pizza', 'Descripcion pizza, Descripcion pizza',
+              19.00, ''),
+          ListPizza(
+              'Japan Pizza', 'Descripcion pizza, Descripcion pizza', 22.99, ''),
+          ListPizza('Hungria Pizza', 'Descripcion pizza, Descripcion pizza',
+              32.25, ''),
+          ListPizza('Hawaiian Pizza', 'Descripcion pizza, Descripcion pizza',
+              19.00, ''),
+          ListPizza(
+              'Japan Pizza', 'Descripcion pizza, Descripcion pizza', 22.99, ''),
         ],
       )),
     );
   }
 }
-
-
